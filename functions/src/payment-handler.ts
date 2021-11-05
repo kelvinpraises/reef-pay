59c8537b500b7138ca1d8123da2102b08a7d699d
Kelvin <kelvinpraises@gmail.com>
2021-11-05 10:00:08 +0100

    feat: updated payment handler

--- functions/src/payment-handler.ts
+++ functions/src/payment-handler.ts
@@ -1,13 +1,14 @@
 import functions = require("firebase-functions");
 import { checkTx, sendTx } from "./utils/reef";
 import { PaymentDoc } from "./utils/types";
-import { callWebHook } from "./utils/payment";
+import { callWebHook, refund } from "./utils/payment";
 
 // When a new payment request is made checkTx
 export default functions.firestore
   .document("/paymentRequest/{documentId}")
   .onCreate(async (snap, context) => {
     const doc = snap.data() as PaymentDoc;
+    functions.logger.log("processing", context.params.documentId, doc);
     await checkTx(doc, paid, unpaid, underPaid, overPaid);
   });
 
@@ -16,7 +17,7 @@ const paid = async (doc: PaymentDoc) => {
 
   callWebHook(callbackUrl!, "payment.paid.success");
 
-  // TODO: recursively call transfer to merchants addrees
+  // TODO: recursively call transfer to merchants address
   sendTx(mnemonic!, address!, amount!)
     .catch(console.error)
     .finally(() => process.exit());
@@ -33,7 +34,9 @@ const underPaid = async (doc: PaymentDoc) => {
 
   callWebHook(callbackUrl!, "payment.underpaid.failed");
 
-  // TODO: call refund
+  const refumdAddress = "";
+  const refundAmount = "";
+  refund(refumdAddress, refundAmount);
 };
 
 const overPaid = async (doc: PaymentDoc) => {
@@ -46,40 +49,7 @@ const overPaid = async (doc: PaymentDoc) => {
     .catch(console.error)
     .finally(() => process.exit());
 
-  // TODO: call refund
+  const refumdAddress = "";
+  const refundAmount = "";
+  refund(refumdAddress, refundAmount);
 };
-
-// // Grab the current value of what was written to Firestore.
-// const original = snap.data().original;
-
-// // Access the parameter `{documentId}` with `context.params`
-// functions.logger.log("Uppercasing", context.params.documentId, original);
-
-// const uppercase = original.toUpperCase();
-
-// // TODO: add merchantId
-// const address = "";
-// const amount = 4;
-// const time = new Date();
-
-// const data = { address, amount, time };
-
-// await checkTx(data, paid, unpaid, underPaid, overPaid);
-
-// // You must return a Promise when performing asynchronous tasks inside a Functions such as
-// // writing to Firestore.
-// // Setting an 'uppercase' field in Firestore document returns a Promise.
-// return snap.ref.set({ uppercase }, { merge: true });
-//
-//
-//
-//
-//
-//
-//
-// const merchantId = "";
-// const address = "";
-// const amount = 4;
-// const time = new Date();
-
-// const doc = { merchantId, address, amount, time };
