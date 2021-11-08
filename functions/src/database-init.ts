f72757d81dc968b6851b1a5ae2fac45d0877c4ea
Kelvin <kelvinpraises@gmail.com>
2021-11-08 09:55:26 +0100

    feat: done with most overlay ui

--- functions/src/database-init.ts
+++ functions/src/database-init.ts
@@ -1,3 +1,2 @@
 // when first user is created this initializes the database
 // to a certain route structure or not, if the route needs are not too complex....
-// done
---
+++ functions/utils/payment.ts
@@ -0,0 +1,53 @@
+import * as https from "https";
+import { WebHookData } from "./types";
+
+export async function callWebHook(callbackUrl: string, data: WebHookData) {
+  const { transactionId, event, amount } = data;
+  const { hostname, pathname } = new URL(callbackUrl);
+
+  const postData = JSON.stringify({
+    data: {
+      id: transactionId,
+      amount,
+      created: Date.now(),
+    },
+    type: event,
+  });
+
+  const options = {
+    method: "POST",
+    hostname: hostname,
+    path: pathname,
+  };
+
+  await new Promise((resolve, reject) => {
+    const req = https.request(options, function (res) {
+      const chunks: any[] = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        try {
+          const body = Buffer.concat(chunks).toString();
+          // TODO: resolve res.statusCode and know wether to retry webhook
+          resolve(body);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      res.on("error", function (error) {
+        console.error(error);
+        reject(error);
+      });
+    });
+
+    req.write(postData);
+
+    req.end();
+  });
+}
+
+export async function refund(address: string, amount: any) {}
